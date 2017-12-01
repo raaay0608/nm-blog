e3ec8a87a30b1e6781b15a6471641602d274892c
changrui0608 <changrui0608@gmail.com>
2017-12-01 10:56:09 +0800

    basic post list page without pagination

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 import KoaRouter from 'koa-router'
 
+import Post from '~/models/post'
+
 export const router = new KoaRouter()
 
 router.get('/', async (ctx, next) => {
   switch (ctx.accepts('html')) {
     case 'html':
-      return ctx.render('index', {})
+      const posts = await Post.list()
+      return ctx.render('index', { posts })
     default:
       ctx.throw(406)
   }
