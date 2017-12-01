ce5d562e8821e145f6f619a9136a5845736d20cd
changrui0608 <changrui0608@gmail.com>
2017-12-01 13:18:10 +0800

    fix auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,7 @@
 
 // must be used after the using of 'koa-jwt'
 export default async (ctx, next) => {
+  // allow any req from admin
   if (ctx.state.user && ctx.state.user.admin) {
     ctx.state.admin = true
     return next()
@@ -8,10 +9,21 @@ export default async (ctx, next) => {
 
   ctx.state.admin = false
 
-  const type = ctx.accepts('json', 'html', 'image/*')
-  if (type === 'json' && ctx.url !== '/token') {
-    // json api is only for admin, thus block it
-    return ctx.throw(401)
+  // allow accessing for /statics/*
+  if (ctx.url.startsWith('/statics')) {
+    return next()
   }
-  return next()
+
+  // allow non-json request
+  const type = ctx.accepts('json', 'html', 'image/*')
+  if (type !== 'json') {
+    return next()
+  }
+
+  // allow get token & check token
+  if (ctx.url === '/token') {
+    return next()
+  }
+
+  return ctx.throw(401)
 }
